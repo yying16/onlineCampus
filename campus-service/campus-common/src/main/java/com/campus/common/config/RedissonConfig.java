5c9bc2e32cd7216e91dd3247651c39daeb4df892
yying <1712131536@qq.com>
2023-08-28 21:14:36 +0800

    消息模块和网关完成分布式修改

--- campus-service/campus-common/src/main/java/com/campus/common/config/RedissonConfig.java
+++ campus-service/campus-common/src/main/java/com/campus/common/config/RedissonConfig.java
@@ -1,26 +1,39 @@
-//package com.campus.common.config;
-//
-//import org.redisson.Redisson;
-//import org.redisson.api.RedissonClient;
-//import org.redisson.config.Config;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//@Configuration
-//public class RedissonConfig {
-//    @Autowired
-//    private RedisProperties redisProperties;
-//
-//    @Bean
-//    public RedissonClient redissonClient() {
-//        Config config = new Config();
-//        config.useSingleServer()
-//                .setAddress("redis://" + redisProperties.getHost() + ":" + redisProperties.getPort())
-//                .setPassword(redisProperties.getPassword())
-//                .setDatabase(redisProperties.getDatabase())
-//                .setConnectionMinimumIdleSize(3);
-//        return Redisson.create(config);
-//    }
-//}
+package com.campus.common.config;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Configuration
+public class RedissonConfig {
+    @Autowired
+    private RedisProperties redisProperties;
+
+    @Bean(name = "redissonClient")
+    public RedissonClient redissonClient() {
+        Config config = new Config();
+//        List<String> notes = redisProperties.getCluster().getNodes();
+        String master = "redis://117.72.15.203:6379";
+        Set<String> slaves = new HashSet() {{
+            add("redis://117.72.14.204:6379");
+            add("redis://117.72.13.205:6379");
+        }};
+        config.useMasterSlaveServers()
+                .setDatabase(redisProperties.getDatabase())
+                .setPassword(redisProperties.getPassword())
+                .setMasterAddress(master)
+                .setSlaveAddresses(slaves); // 设置集群节点扫描间隔，单位毫秒，可选设置
+        return Redisson.create(config);
+    }
+
+}
