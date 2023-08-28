c331e03764c5d78e1185d71b18ef5011aaf6d951
yying <1712131536@qq.com>
2023-08-28 18:10:44 +0800

    消息模块分布式锁修改完成

--- campus-service/campus-message/src/main/java/com/campus/message/MessageApplication.java
+++ campus-service/campus-message/src/main/java/com/campus/message/MessageApplication.java
@@ -1,11 +1,15 @@
 package com.campus.message;
 
 import com.campus.common.util.SpringContextUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -14,9 +18,21 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableDiscoveryClient
 @EnableScheduling
 @EnableAsync
+@Slf4j
 public class MessageApplication {
+
+    @Value("${serverId}")
+    String serverId;
+
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(MessageApplication.class, args);
         SpringContextUtil.setApplicationContext(context);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner() {
+        return args -> {
+            log.info("【服务id】 {}",serverId);
+        };
     }
 }
