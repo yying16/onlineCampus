0d2654777fe3b7ceb7a1f29100ae5dc77928a019
yying <1712131536@qq.com>
2023-08-30 18:55:35 +0800

    修改redission

--- campus-service/campus-common/src/main/java/com/campus/common/config/RedissonConfig.java
+++ campus-service/campus-common/src/main/java/com/campus/common/config/RedissonConfig.java
@@ -8,32 +8,19 @@ import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
-
 @Configuration
 public class RedissonConfig {
     @Autowired
     private RedisProperties redisProperties;
 
-    @Bean(name = "redissonClient")
+    @Bean
     public RedissonClient redissonClient() {
         Config config = new Config();
-//        List<String> notes = redisProperties.getCluster().getNodes();
-        String master = "redis://117.72.15.203:6379";
-        Set<String> slaves = new HashSet() {{
-            add("redis://117.72.14.204:6379");
-            add("redis://117.72.13.205:6379");
-        }};
-        config.useMasterSlaveServers()
-                .setDatabase(redisProperties.getDatabase())
+        config.useSingleServer()
+                .setAddress("redis://" + redisProperties.getHost() + ":" + redisProperties.getPort())
                 .setPassword(redisProperties.getPassword())
-                .setMasterAddress(master)
-                .setSlaveAddresses(slaves); // 设置集群节点扫描间隔，单位毫秒，可选设置
+                .setDatabase(redisProperties.getDatabase())
+                .setConnectionMinimumIdleSize(3);
         return Redisson.create(config);
     }
-
 }
