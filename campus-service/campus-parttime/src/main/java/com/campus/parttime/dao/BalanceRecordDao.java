d5bea3276965cd368c97fc9671a91bbf59294aa4
yying <1712131536@qq.com>
2023-09-01 19:26:18 +0800

    0

--- campus-service/campus-parttime/src/main/java/com/campus/parttime/dao/BalanceRecordDao.java
+++ campus-service/campus-parttime/src/main/java/com/campus/parttime/dao/BalanceRecordDao.java
@@ -1,5 +1,7 @@
 package com.campus.parttime.dao;
 
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.campus.parttime.domain.BalanceRecord;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
@@ -7,7 +9,7 @@ import org.apache.ibatis.annotations.Update;
 import java.math.BigDecimal;
 
 @Mapper
-public interface BalanceRecordDao {
+public interface BalanceRecordDao extends BaseMapper<BalanceRecord> {
     @Update("update t_user set balance=balance-#{money} where user_id=#{userId}")
     void payJob(String userId, BigDecimal money);
 
