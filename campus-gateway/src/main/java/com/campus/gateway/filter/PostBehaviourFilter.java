abd39e0d2a8bd9dde07cc1d1e89744f48df09d09
yying <1712131536@qq.com>
2023-09-02 16:09:37 +0800

    关掉两个不必要的过滤器

--- campus-gateway/src/main/java/com/campus/gateway/filter/PostBehaviourFilter.java
+++ campus-gateway/src/main/java/com/campus/gateway/filter/PostBehaviourFilter.java
@@ -1,50 +1,50 @@
-package com.campus.gateway.filter;
-
-import com.alibaba.fastjson.JSONObject;
-import com.campus.gateway.domain.Behaviour;
-import com.campus.gateway.service.BehaviourService;
-import com.campus.gateway.util.TimeUtil;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.gateway.filter.GatewayFilterChain;
-import org.springframework.cloud.gateway.filter.GlobalFilter;
-import org.springframework.core.annotation.Order;
-import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.http.server.reactive.ServerHttpResponse;
-import org.springframework.stereotype.Component;
-import org.springframework.web.server.ServerWebExchange;
-import reactor.core.publisher.Mono;
-
-import java.util.Objects;
-
-/**
- * 后置行为过滤器
- */
-@Component
-@Slf4j
-@Order(2)
-public class PostBehaviourFilter implements GlobalFilter {
-
-    @Autowired
-    StringRedisTemplate redisTemplate;
-
-    @Autowired
-    BehaviourService behaviourService;
-
-    @Override
-    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-        return chain.filter(exchange).then(Mono.fromRunnable(() -> {
-            log.info("post {}", System.currentTimeMillis());
-            String bid = exchange.getRequest().getHeaders().getFirst("bid");
-            String behaviourStr = redisTemplate.opsForValue().get("behaviour"+bid);
-            Behaviour behaviour = JSONObject.parseObject(behaviourStr, Behaviour.class);
-            ServerHttpResponse response = exchange.getResponse();
-            behaviour.setStatus(response.getRawStatusCode());
-            behaviour.setReasonPhrase(Objects.requireNonNull(response.getStatusCode()).getReasonPhrase());
-            behaviour.setResponseTime(TimeUtil.getCurrentTime());
-            behaviour.setDuration(TimeUtil.getTimeInterval(behaviour.getRequestTime(),behaviour.getResponseTime()));
-            behaviourService.asyncInsert(behaviour);
-            redisTemplate.delete("behaviour"+bid);
-        }));
-    }
-}
+//package com.campus.gateway.filter;
+//
+//import com.alibaba.fastjson.JSONObject;
+//import com.campus.gateway.domain.Behaviour;
+//import com.campus.gateway.service.BehaviourService;
+//import com.campus.gateway.util.TimeUtil;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+//import org.springframework.cloud.gateway.filter.GlobalFilter;
+//import org.springframework.core.annotation.Order;
+//import org.springframework.data.redis.core.StringRedisTemplate;
+//import org.springframework.http.server.reactive.ServerHttpResponse;
+//import org.springframework.stereotype.Component;
+//import org.springframework.web.server.ServerWebExchange;
+//import reactor.core.publisher.Mono;
+//
+//import java.util.Objects;
+//
+///**
+// * 后置行为过滤器
+// */
+//@Component
+//@Slf4j
+//@Order(2)
+//public class PostBehaviourFilter implements GlobalFilter {
+//
+//    @Autowired
+//    StringRedisTemplate redisTemplate;
+//
+//    @Autowired
+//    BehaviourService behaviourService;
+//
+//    @Override
+//    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+//        return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+//            log.info("post {}", System.currentTimeMillis());
+//            String bid = exchange.getRequest().getHeaders().getFirst("bid");
+//            String behaviourStr = redisTemplate.opsForValue().get("behaviour"+bid);
+//            Behaviour behaviour = JSONObject.parseObject(behaviourStr, Behaviour.class);
+//            ServerHttpResponse response = exchange.getResponse();
+//            behaviour.setStatus(response.getRawStatusCode());
+//            behaviour.setReasonPhrase(Objects.requireNonNull(response.getStatusCode()).getReasonPhrase());
+//            behaviour.setResponseTime(TimeUtil.getCurrentTime());
+//            behaviour.setDuration(TimeUtil.getTimeInterval(behaviour.getRequestTime(),behaviour.getResponseTime()));
+//            behaviourService.asyncInsert(behaviour);
+//            redisTemplate.delete("behaviour"+bid);
+//        }));
+//    }
+//}
--- campus-gateway/src/main/java/com/campus/gateway/filter/PreBehaviourFilter.java
+++ campus-gateway/src/main/java/com/campus/gateway/filter/PreBehaviourFilter.java
@@ -1,79 +1,79 @@
-package com.campus.gateway.filter;
-
-import com.alibaba.fastjson.JSONObject;
-import com.baomidou.mybatisplus.core.toolkit.IdWorker;
-import com.campus.gateway.domain.Behaviour;
-import com.campus.gateway.util.TimeUtil;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.gateway.filter.GatewayFilterChain;
-import org.springframework.cloud.gateway.filter.GlobalFilter;
-import org.springframework.core.annotation.Order;
-import org.springframework.core.io.buffer.DataBuffer;
-import org.springframework.core.io.buffer.DataBufferUtils;
-import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.server.reactive.ServerHttpRequest;
-import org.springframework.stereotype.Component;
-import org.springframework.web.server.ServerWebExchange;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
-
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-/**
- * 前置行为过滤器
- */
-@Component
-@Slf4j
-@Order(3)
-public class PreBehaviourFilter implements GlobalFilter {
-
-    @Autowired
-    StringRedisTemplate redisTemplate;
-
-    @Override
-    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-        log.info("pre {}", System.currentTimeMillis());
-        Behaviour behaviour = new Behaviour();
-        ServerHttpRequest request = exchange.getRequest();
-        behaviour.setBehaviourId(IdWorker.getIdStr(behaviour));
-        behaviour.setUri(request.getURI().toString());
-        behaviour.setHeader(JSONObject.toJSONString(request.getHeaders()));
-        behaviour.setMethod(request.getMethodValue());
-        behaviour.setUid(request.getHeaders().getFirst("uid"));
-        behaviour.setParams(JSONObject.toJSONString(request.getQueryParams()));
-        behaviour.setPath(request.getURI().getPath());
-        behaviour.setService(getServiceName(behaviour.getPath()));
-        behaviour.setRequestTime(TimeUtil.getCurrentTime());
-        redisTemplate.opsForValue().set("behaviour" + behaviour.getBehaviourId(), JSONObject.toJSONString(behaviour)); // 将行为存放在redis中
-        //修改请求头
-        HttpHeaders customHeaders = new HttpHeaders();
-        customHeaders.add("bid", behaviour.getBehaviourId());
-        ServerHttpRequest requestWithCustomHeaders = request.mutate()
-                .headers(httpHeaders -> httpHeaders.addAll(customHeaders))
-                .build();
-        ServerWebExchange exchangeWithCustomHeaders = exchange.mutate().request(requestWithCustomHeaders).build();
-        return chain.filter(exchangeWithCustomHeaders);
-    }
-
-    /**
-     * 根据uri获取服务名
-     */
-    private String getServiceName(String uri) {
-        try {
-            String input = uri;
-            String regex = "campus[A-Z|a-z]+"; // 匹配电话号码的正则表达式
-            Pattern pattern = Pattern.compile(regex);
-            Matcher matcher = pattern.matcher(input);
-            matcher.find();
-            String service = matcher.group(); // 获取匹配的子串
-            return service;
-        } catch (Exception e) {
-            return "UnKnown";
-        }
-    }
-}
+//package com.campus.gateway.filter;
+//
+//import com.alibaba.fastjson.JSONObject;
+//import com.baomidou.mybatisplus.core.toolkit.IdWorker;
+//import com.campus.gateway.domain.Behaviour;
+//import com.campus.gateway.util.TimeUtil;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+//import org.springframework.cloud.gateway.filter.GlobalFilter;
+//import org.springframework.core.annotation.Order;
+//import org.springframework.core.io.buffer.DataBuffer;
+//import org.springframework.core.io.buffer.DataBufferUtils;
+//import org.springframework.data.redis.core.StringRedisTemplate;
+//import org.springframework.http.HttpHeaders;
+//import org.springframework.http.server.reactive.ServerHttpRequest;
+//import org.springframework.stereotype.Component;
+//import org.springframework.web.server.ServerWebExchange;
+//import reactor.core.publisher.Flux;
+//import reactor.core.publisher.Mono;
+//
+//import java.io.IOException;
+//import java.nio.charset.StandardCharsets;
+//import java.util.regex.Matcher;
+//import java.util.regex.Pattern;
+//
+///**
+// * 前置行为过滤器
+// */
+//@Component
+//@Slf4j
+//@Order(3)
+//public class PreBehaviourFilter implements GlobalFilter {
+//
+//    @Autowired
+//    StringRedisTemplate redisTemplate;
+//
+//    @Override
+//    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+//        log.info("pre {}", System.currentTimeMillis());
+//        Behaviour behaviour = new Behaviour();
+//        ServerHttpRequest request = exchange.getRequest();
+//        behaviour.setBehaviourId(IdWorker.getIdStr(behaviour));
+//        behaviour.setUri(request.getURI().toString());
+//        behaviour.setHeader(JSONObject.toJSONString(request.getHeaders()));
+//        behaviour.setMethod(request.getMethodValue());
+//        behaviour.setUid(request.getHeaders().getFirst("uid"));
+//        behaviour.setParams(JSONObject.toJSONString(request.getQueryParams()));
+//        behaviour.setPath(request.getURI().getPath());
+//        behaviour.setService(getServiceName(behaviour.getPath()));
+//        behaviour.setRequestTime(TimeUtil.getCurrentTime());
+//        redisTemplate.opsForValue().set("behaviour" + behaviour.getBehaviourId(), JSONObject.toJSONString(behaviour)); // 将行为存放在redis中
+//        //修改请求头
+//        HttpHeaders customHeaders = new HttpHeaders();
+//        customHeaders.add("bid", behaviour.getBehaviourId());
+//        ServerHttpRequest requestWithCustomHeaders = request.mutate()
+//                .headers(httpHeaders -> httpHeaders.addAll(customHeaders))
+//                .build();
+//        ServerWebExchange exchangeWithCustomHeaders = exchange.mutate().request(requestWithCustomHeaders).build();
+//        return chain.filter(exchangeWithCustomHeaders);
+//    }
+//
+//    /**
+//     * 根据uri获取服务名
+//     */
+//    private String getServiceName(String uri) {
+//        try {
+//            String input = uri;
+//            String regex = "campus[A-Z|a-z]+"; // 匹配电话号码的正则表达式
+//            Pattern pattern = Pattern.compile(regex);
+//            Matcher matcher = pattern.matcher(input);
+//            matcher.find();
+//            String service = matcher.group(); // 获取匹配的子串
+//            return service;
+//        } catch (Exception e) {
+//            return "UnKnown";
+//        }
+//    }
+//}
