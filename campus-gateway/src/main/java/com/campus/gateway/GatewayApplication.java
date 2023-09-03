88a7c2f0cd4e488d4ddee77c01b0e8a073ed26b4
yying <1712131536@qq.com>
2023-09-03 15:44:26 +0800

    修改Redisson

--- campus-gateway/src/main/java/com/campus/gateway/GatewayApplication.java
+++ campus-gateway/src/main/java/com/campus/gateway/GatewayApplication.java
@@ -1,12 +1,9 @@
 package com.campus.gateway;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 import java.net.InetAddress;
